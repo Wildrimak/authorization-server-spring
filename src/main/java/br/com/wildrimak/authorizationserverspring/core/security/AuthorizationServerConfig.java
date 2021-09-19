b9bf8e5a9416455d57919345276e339c51d2b0bf
Wildrimak <finalwildrimak@gmail.com>
2021-09-19 15:58:48 -0300

    Configura forma de acesso ao check token

--- src/main/java/br/com/wildrimak/authorizationserverspring/core/security/AuthorizationServerConfig.java
+++ src/main/java/br/com/wildrimak/authorizationserverspring/core/security/AuthorizationServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -41,6 +42,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override // Somente o fluxo password flow precisa disso
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.authenticationManager(authenticationManager);
+    }
+    
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+
+//	security.checkTokenAccess("isAuthenticated()");
+	security.checkTokenAccess("permitAll()");
     }
 
 }
