022d20c2b1fce535334334762f4f3c55800a370d
Wildrimak <finalwildrimak@gmail.com>
2021-09-19 15:05:39 -0300

    Configura os Resource Owners permitidos

--- src/main/java/br/com/wildrimak/authorizationserverspring/core/security/WebSecurityConfig.java
+++ src/main/java/br/com/wildrimak/authorizationserverspring/core/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.wildrimak.authorizationserverspring.core.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,4 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
 	return new BCryptPasswordEncoder();
     }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+       return super.authenticationManagerBean();
+    }
+
 }
