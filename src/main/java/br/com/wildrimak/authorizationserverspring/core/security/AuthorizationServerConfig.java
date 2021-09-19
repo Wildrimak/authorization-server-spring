5539b3e54f843d25ec7802ea48601c97149647f3
Wildrimak <finalwildrimak@gmail.com>
2021-09-19 15:05:03 -0300

    Configura clients permitidos

--- src/main/java/br/com/wildrimak/authorizationserverspring/core/security/AuthorizationServerConfig.java
+++ src/main/java/br/com/wildrimak/authorizationserverspring/core/security/AuthorizationServerConfig.java
@@ -1,11 +1,46 @@
 package br.com.wildrimak.authorizationserverspring.core.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+	clients
+		.inMemory()
+        		.withClient("wildrimak-web")
+        		.secret(passwordEncoder.encode("wdk123"))
+        		.authorizedGrantTypes("password")
+        		.scopes("write", "read")
+        		.accessTokenValiditySeconds(300)
+        	.and()
+                	.withClient("wildrimak-mobile")
+        		.secret(passwordEncoder.encode("wdk123"))
+        		.authorizedGrantTypes("password")
+        		.scopes("write", "read")
+        ;
+    
+    }
+    
+    @Override // Somente o fluxo password flow precisa disso
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
 
 }
